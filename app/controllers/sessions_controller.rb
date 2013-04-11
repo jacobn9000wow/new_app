06d5371bdd541b35415f491e875c2b5888d7b7b5
jacobn9000wow <jacob_n90wow@hotmail.com>
2013-04-11 00:02:18 -0700

    json - redirect to user after sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_back_or user #http://ruby.railstutorial.org/chapters/updating-showing-and-deleting-users#code-friendly_session_create
+      #redirect_back_or user #http://ruby.railstutorial.org/chapters/updating-showing-and-deleting-users#code-friendly_session_create
+      redirect_to user
     else
       # Create an error message and re-render the signin form.
       flash.now[:error] = 'Invalid username/password combination'
